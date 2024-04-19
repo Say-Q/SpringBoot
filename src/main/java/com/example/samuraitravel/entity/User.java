7477db320a690d40e688939b537b3d751a286aeb
Say-Q <say19840702@gmail.com>
2024-04-19 13:12:58 +0900

    第19章まで

--- src/main/java/com/example/samuraitravel/entity/User.java
+++ src/main/java/com/example/samuraitravel/entity/User.java
@@ -1,20 +1,21 @@
 package com.example.samuraitravel.entity;
 
-import java.sql.Timestamp;
+import java.security.Timestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "houses")
+@Table(name = "users")
 @Data
-
-public class House {
+public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
@@ -23,17 +24,8 @@ public class House {
 	@Column(name = "name")
 	private String name;
 	
-	@Column(name = "image_name")
-	private String imageName;
-	
-	@Column(name = "description")
-	private String description;
-	
-	@Column(name = "price")
-	private Integer price;
-	
-	@Column(name = "capacity")
-	private Integer capacity;
+	@Column(name = "furigana")
+	private String furigana;
 	
 	@Column(name = "postal_code")
 	private String postalCode;
@@ -44,10 +36,23 @@ public class House {
 	@Column(name = "phone_number")
 	private String phoneNumber;
 	
-	@Column(name = "created_at",insertable = false,updatable = false)
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@ManyToOne
+	@JoinColumn(name = "role_id")
+	private Role role;
+	
+	@Column(name = "enabled")
+	private Boolean enabled;
+	
+	@Column(name = "created_at", insertable = false, updatable = false)
 	private Timestamp createdAt;
 	
-	@Column(name = "updated_at",insertable = false,updatable = false)
+	@Column(name = "updated_at", insertable = false, updatable = false)
 	private Timestamp updatedAt;
 	
 }
